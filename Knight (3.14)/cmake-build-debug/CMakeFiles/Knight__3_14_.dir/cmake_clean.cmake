f797aa73dc3bb16e1964008bd87e3cedc3e6a3f8
Cosmin Stretea <cosmin.stretea@gmail.com>
2020-01-04 01:11:23 +0200

    init commit

---
+++ Knight (3.14)/cmake-build-debug/CMakeFiles/Knight__3_14_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Knight__3_14_.dir/main.c.obj"
+  "Knight__3_14_.exe"
+  "Knight__3_14_.exe.manifest"
+  "Knight__3_14_.pdb"
+  "libKnight__3_14_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Knight__3_14_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
